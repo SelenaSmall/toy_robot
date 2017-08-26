f03ae4b51dc9ced1b9d882b5710ee7065b68b479
SelenaSmall <selenawiththetattoo@gmail.com>
2017-08-26 13:49:43 +1200

    Main loop for reading user inputs

--- toy_robot.rb
+++ toy_robot.rb
@@ -7,3 +7,17 @@ require_relative './lib/table'
 
 # Is this a valid position on the table
 puts @table.valid_position?(1, 0)
+
+# Keep reading user inputs while the program is running
+loop do
+  puts 'Select your command'
+  input = gets.chomp
+
+  unless input == 'EXIT'
+    puts 'Robot has been placed' if input == 'PLACE'
+    next
+  end
+
+  puts 'Goodbye!'
+  break
+end
