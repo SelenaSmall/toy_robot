b39456f4fc8a632a678e8856b60792641c9e7f3f
SelenaSmall <selenawiththetattoo@gmail.com>
2017-08-26 14:21:33 +1200

    Validate the position of the placed robot against the table coordinates

--- toy_robot.rb
+++ toy_robot.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/ruby
 
+require_relative './lib/table'
 require_relative './lib/robot'
 
+table = Table.new
 robot = Robot.new
 
 # Keep reading user inputs while the program is running
@@ -11,9 +13,10 @@ loop do
 
   unless input == 'EXIT'
     position = robot.place(input)
-    puts "Position: #{position.x},#{position.y}"
+    # Only update robot if the position is valid on the table
+    robot.update_robot(position) if table.valid_position?(position.x, position.y)
 
-    robot.update_robot(position)
+    puts "Position: #{position.x},#{position.y}"
 
     puts 'placed' unless robot.not_in_place?
     next
